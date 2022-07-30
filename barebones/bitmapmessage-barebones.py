0db60b2898658d6a2cc5ffd5392ed24fab054223
nihil <106533876+faatemerajani@users.noreply.github.com>
2022-07-30 09:33:16 +0430

    Update bitmapmessage-barebones.py

--- barebones/bitmapmessage-barebones.py
+++ barebones/bitmapmessage-barebones.py
@@ -1,7 +1,7 @@
 """Bitmap Message (barebones version)
 by Al Sweigart al@inventwithpython.com
 Displays a text message according to the provided bitmap image."""
-import sys
+import sys, pyperclip
 
 bitmap = """
         ********
@@ -28,14 +28,19 @@ print('Bitmap Message, by Al Sweigart al@inventwithpython.com')
 print('Enter the message to display with the bitmap.')
 message = input('> ')
 
+# create a shaped massage out of msg:
+shapedMsg = """ """
 # Loop over each line in the multi-line bitmap:
 for line in bitmap.splitlines():
     # Loop over each character in the line:
     for i in range(len(line)):
         if line[i] == ' ':
-            # Print an empty space since there's a space in the bitmap:
-            print(' ', end='')
+            # add an empty space since there's a space in the bitmap:
+            shapedMsg += ' '
         else:
-            # Print a character from the message:
-            print(message[i % len(message)], end='')
-    print()  # Print a newline.
+            # append character from the message:
+            shapedMsg += message[i % len(message)]
+    # end the line:
+    shapedMsg += '\n'
+# copy to clipboard to use elsewhere
+pyperclip.copy(shapedMsg)
